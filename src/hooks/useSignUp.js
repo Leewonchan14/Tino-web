36b713a630a760b49da8a534a9d89091cc0c1bd5
Won Chan Lee <twoone14@tukorea.ac.kr>
2024-05-05 22:05:29 +0900

    [feat] 회원가입 유효성 검사 코드 추가

---
+++ src/hooks/useSignUp.js
@@ -0,0 +1,107 @@
+import {useState} from "react";
+
+const MAJOR = [
+    {value: "소프트웨어학과"},
+    {value: "컴퓨터공학과"},
+    {value: "인공지능학과"},
+    {value: "게임공학과"},
+    {value: "산업경영학과"},
+    {value: "IT경영학과"},
+    {value: "데이터사이언스경영학과"},
+    {value: "글로벌융합공학과"},
+    {value: "기계공학과"},
+    {value: "기계설계공학과"},
+    {value: "나노반도체공학과"},
+    {value: "메카트로닉스학과"},
+    {value: "AI로봇학과"},
+    {value: "산업디자인공학과"},
+    {value: "미디어디자인학과"},
+    {value: "생명화학공학과"},
+    {value: "신소재공학과"},
+    {value: "에너지전기공학과"},
+    {value: "지식융합학과"},
+    {value: "전자공학과"},
+    {value: "임베디드시스템학과"},
+];
+
+export const useSignUp = () => {
+    const [signupFormState, setSignupFormState] = useState({
+        email: "",
+        password: "",
+        nickname: "",
+        major: MAJOR[0].value,
+        code: ""
+    })
+
+    const [isValid, setIsValid] = useState({
+        email: true,
+        email_blank: true,
+        password: true,
+        password_blank: true,
+        rePassword: true,
+        nickname: true,
+        nickname_blank: true,
+        code: true
+    })
+
+    const regs = {
+        email: /^[a-zA-Z0-9._%+-]+@tukorea.ac.kr$/,
+        password: /^(?=.*[a-zA-Z])(?=.*[0-9]).{8,}$/,
+        notBlank: /^.+$/,
+        code: /^[0-9]{6}$/
+    }
+
+    const [rePassword, setRePassword] = useState("");
+
+    const onSubmit = () => {
+        setIsValid((state) => {
+                let newState = {
+                    ...isValid,
+                    email: regs.email.test(signupFormState.email),
+                    email_blank: regs.notBlank.test(signupFormState.email),
+                    password: regs.password.test(signupFormState.password),
+                    password_blank: regs.notBlank.test(signupFormState.password),
+                    nickname_blank: regs.notBlank.test(signupFormState.nickname),
+                    code: regs.code.test(signupFormState.code),
+                }
+
+
+                const allValid = Object.entries(newState).every(([key, value]) => {
+                    return value
+                });
+
+                if (!allValid) {
+                    console.log("입력값이 올바르지 않습니다.")
+                }
+                console.log("회원가입 요청")
+                return newState
+            }
+        )
+    }
+
+
+    const onChange = (e) => {
+        if (e.target.name === "rePassword") {
+            setRePassword(e.target.value)
+            setIsValid({
+                ...isValid,
+                rePassword: signupFormState.password === e.target.value
+            })
+            return
+        }
+
+        setSignupFormState({
+            ...signupFormState,
+            [e.target.name]: e.target.value
+        })
+
+        if (e.target.name === "password") {
+            setIsValid({
+                ...isValid,
+                rePassword: rePassword === e.target.value
+            })
+        }
+    }
+
+    return {signupFormState, rePassword, MAJOR, isValid, onChange, onSubmit};
+}
--- src/pages/SignUp.js
+++ src/pages/SignUp.js
@@ -1,24 +1,63 @@
 import React from 'react';
-import IdPasswordComp from "../components/IdPasswordComp";
+import {ComboBox, InputComp} from "../components/InputComp";
 import ButtonComp from "../components/ButtonComp";
+import {useSignUp} from "../hooks/useSignUp";
+
+const SIGNUP_PATH = "/signup";
 
 function SignUp(props) {
+
+    const {
+        signupFormState,
+        rePassword,
+        MAJOR,
+        isValid,
+        onChange,
+        onSubmit
+
+    } = useSignUp();
+
     return (
         <div className={"h-[70%] flex justify-center"}>
             <div className={"w-[50%] "}>
-                <h1 className={"text-6xl my-12 font-bold flex justify-center"}
-                    style={{fontFamily: "GmarketSans"}}>이메일 인증
+                <h1 className={"text-6xl my-5 font-bold flex justify-center"}
+                    style={{fontFamily: "GmarketSans"}}>회원 가입
                 </h1>
-                <span className={"flex justify-center"}>@tukorea.ac.kr 인 도메인만 사용 가능합니다.</span>
-                <div className={"flex"}>
-                    <IdPasswordComp placeholder={"E-mail 을 입력하세요"}/>
-                    <div className={"w-[20%]"}>
-                        <ButtonComp text={"인증코드 발송"} className={"!text-sm"}/>
-                    </div>
+                <div className={"flex-col"}>
+                    <InputComp value={signupFormState.email} name={"email"} onChange={onChange}
+                               placeholder={"인증코드를 보낸 이메일을 입력해 주세요"}/>
+                    <span style={{color: "red"}}>{!isValid.email_blank && "이메일을 입력해주세요."}</span>
+                    <span style={{color: "red"}}>{!isValid.email && "이메일 형식이 올바르지 않습니다."}</span>
+
+
+                    <InputComp value={signupFormState.code} name={"code"} onChange={onChange}
+                               placeholder={"E-mail로 받은 인증코드를 입력하세요"}/>
+                    <span style={{color: "red"}}>{!isValid.code && "인증코드는 6자리 숫자입니다."}</span>
+
+
+                    <InputComp value={signupFormState.password} name={"password"} onChange={onChange}
+                               placeholder={"비밀번호를 입력하세요"}/>
+                    <span style={{color: "red"}}>{!isValid.password && "비밀번호는 8자리 이상, 영문과 숫자를 포함해야 합니다."}</span>
+                    <span style={{color: "red"}}>{!isValid.password_blank && "비밀번호를 입력해주세요."}</span>
+
+
+                    <InputComp value={rePassword} name={"rePassword"} onChange={onChange}
+                               placeholder={"비밀번호를 다시 입력하세요"}/>
+                    {!isValid.rePassword && <span style={{color: "red"}}>비밀번호가 일치하지 않습니다.</span>}
+
+
+                    <InputComp value={signupFormState.nickname} name={"nickname"} onChange={onChange}
+                               placeholder={"이름을 입력하세요"}/>
+                    <span style={{color: "red"}}>{!isValid.nickname_blank && "이름을 입력해주세요."}</span>
+
+
+                    <ComboBox value={signupFormState.major} onChange={onChange} options={MAJOR}/>
+                    <ButtonComp text={"회원가입"} onClick={onSubmit}/>
                 </div>
+
             </div>
         </div>
     );
 }
 
-export default SignUp;
+export {SignUp, SIGNUP_PATH};
