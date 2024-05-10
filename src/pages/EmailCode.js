ab8dbe533ddbcba820fbb26205f5333e9b4819a1
Won Chan Lee <twoone14@tukorea.ac.kr>
2024-05-10 15:14:05 +0900

    [feat] 티노 아이콘 추가

--- src/pages/EmailCode.js
+++ src/pages/EmailCode.js
@@ -6,10 +6,11 @@ import {G_MARKET_FONT} from "../constant/FontFamily";
 const EMAIL_PAGE_PATH = "/email/auth";
 
 
-function SignUpButton({isFetching, text, onClick}) {
+function SignUpButton({isFetching, text, onClick, className}) {
     return (
         <button disabled={isFetching} style={{fontFamily: G_MARKET_FONT}}
-                className={"w-full h-[3.5rem] p-[5px] my-[10px] bg-blue-600 text-white font-bold text-xl border-1 rounded-xl"}
+                className={"w-full h-[3.5rem] p-[5px] my-[10px] bg-blue-600 text-white font-bold text-xl border-1 rounded-xl "
+                    + className}
                 onClick={onClick}>
             {!isFetching && text}
             {isFetching && "전송중..."}
@@ -30,12 +31,10 @@ function EmailCode(props) {
                 <span className={"flex justify-center"}>@tukorea.ac.kr 인 도메인만 사용 가능합니다.</span>
                 <div className={"flex"}>
                     <InputComp value={email} onChange={onChangeEmail} placeholder={"E-mail 을 입력하세요"}/>
-                    <div className={"w-[20%] ml-12"}>
-                        <SignUpButton isFetching={isFetching}
-                                      onClick={onClickSendEmail}
-                                      text={"코드 발송"}
-                                      className={"!text-sm"}/>
-                    </div>
+                    <SignUpButton isFetching={isFetching}
+                                  onClick={onClickSendEmail}
+                                  text={"코드 발송"}
+                                  className={"!text-lg !w-32 !ml-8"}/>
                 </div>
 
                 <div style={{fontFamily: G_MARKET_FONT}}
