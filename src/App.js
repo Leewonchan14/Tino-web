3883810ace6f8d064e4bfbcd4b8d72403f13f2df
Won Chan Lee <twoone14@tukorea.ac.kr>
2024-05-20 18:41:21 +0900

    [feat] react-query 를 위한 설정

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
 import './App.css';
+import ScrollToTop from "./hooks/useScrollToTop";
+import AutoLogin from "./pages/AutoLogin";
+import {BrowserRouter, Route, Routes} from "react-router-dom";
+import LayoutWithNavBar from "./layout/LayoutWithNavBar";
+import {Home, HOME_PATH} from "./pages/Home";
+import MyLocation, {MY_LOCATION_PATH} from "./pages/MyLocation";
+import {GAME_PATH, GameDetailPage} from "./pages/GameDetailPage";
+import {Login, LOGIN_PATH} from "./pages/Login";
+import {EMAIL_PAGE_PATH, EmailCode} from "./pages/EmailCode";
+import {SignUp, SIGNUP_PATH} from "./pages/SignUp";
+import {Ranking, RANKING_PATH} from "./pages/Ranking";
+import {FRIEND_PAGE_PATH, FriendPage} from "./pages/FriendPage";
+import React from "react";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <BrowserRouter>
+            <ScrollToTop/>
+            <AutoLogin/>
+            <Routes>
+                <Route exact path="/" element={<LayoutWithNavBar/>}>
+                    <Route exact path={HOME_PATH} element={<Home/>}/>
+                    <Route exact path={MY_LOCATION_PATH} element={<MyLocation/>}/>
+                    <Route exact path={GAME_PATH} element={<GameDetailPage/>}/>
+                    <Route exact path={LOGIN_PATH} element={<Login/>}/>
+                    <Route exact path={EMAIL_PAGE_PATH} element={<EmailCode/>}/>
+                    <Route exact path={SIGNUP_PATH} element={<SignUp/>}/>
+                    <Route exact path={RANKING_PATH} element={<Ranking/>}/>
+                    <Route exact path={FRIEND_PAGE_PATH} element={<FriendPage/>}/>
+                </Route>
+            </Routes>
+        </BrowserRouter>
+    );
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -2,37 +2,19 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
-import {BrowserRouter, Route, Routes} from "react-router-dom";
-import LayoutWithNavBar from "./layout/LayoutWithNavBar";
-import {Home, HOME_PATH} from "./pages/Home";
-import {GAME_PATH, GameDetailPage} from "./pages/GameDetailPage";
-import {Login, LOGIN_PATH} from "./pages/Login";
-import {SignUp, SIGNUP_PATH} from "./pages/SignUp";
-import {EmailCode, EMAIL_PAGE_PATH} from "./pages/EmailCode";
-import {Ranking, RANKING_PATH} from "./pages/Ranking";
-import {FRIEND_PAGE_PATH, FriendPage} from "./pages/FriendPage";
-import ScrollToTop from "./hooks/useScrollToTop";
-import MyLocation, {MY_LOCATION_PATH} from "./pages/MyLocation";
-import AutoLogin from "./pages/AutoLogin";
+import App from "./App";
+import {QueryClient, QueryClientProvider} from '@tanstack/react-query'
+import {ReactQueryDevtools} from "@tanstack/react-query-devtools";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
+const queryClient = new QueryClient();
+
 root.render(
-    <BrowserRouter>
-        <ScrollToTop/>
-        <AutoLogin/>
-        <Routes>
-            <Route exact path="/" element={<LayoutWithNavBar/>}>
-                <Route exact path={HOME_PATH} element={<Home/>}/>
-                <Route exact path={MY_LOCATION_PATH} element={<MyLocation/>}/>
-                <Route exact path={GAME_PATH} element={<GameDetailPage/>}/>
-                <Route exact path={LOGIN_PATH} element={<Login/>}/>
-                <Route exact path={EMAIL_PAGE_PATH} element={<EmailCode/>}/>
-                <Route exact path={SIGNUP_PATH} element={<SignUp/>}/>
-                <Route exact path={RANKING_PATH} element={<Ranking/>}/>
-                <Route exact path={FRIEND_PAGE_PATH} element={<FriendPage/>}/>
-            </Route>
-        </Routes>
-    </BrowserRouter>
+    <QueryClientProvider client={queryClient}>
+        <ReactQueryDevtools /> {/* 선택 사항 */}
+        <App/>
+    </QueryClientProvider>
 );
 
 // If you want to start measuring performance in your app, pass a function
