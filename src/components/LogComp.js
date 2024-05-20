dba84b07bf4198b2c150b002a91fb09c4212a6fe
Won Chan Lee <twoone14@tukorea.ac.kr>
2024-05-20 19:38:29 +0900

    [fix] useGetOneGame stale, gc Time 5분으로 설정

--- src/components/LogComp.js
+++ src/components/LogComp.js
@@ -43,7 +43,7 @@ const OwnLogCardComp = ({gameId, userId}) => {
     }
 
     return (
-        <>
+        <div className={"w-52 flex justify-center items-center"}>
             {isFetching && <LoadingSpinner/>}
 
             {/*<OwnLogComp/>*/}
@@ -56,7 +56,7 @@ const OwnLogCardComp = ({gameId, userId}) => {
             }
 
             <div className={"border-2 h-full"}></div>
-        </>
+        </div>
     )
 }
 
@@ -68,8 +68,8 @@ function LogCardComp({log, index, className}) {
                 <div className={"text-3xl w-20 text-center"}>{index + 1}등</div>
                 <div className={"rounded-full border-2 mx-3"}>
                     <img className={"h-16"}
-                        src={"https://tinos-images-storage.s3.ap-northeast-2.amazonaws.com/default_user_image.png"}
-                         // src={TinoIcon}
+                         src={"https://tinos-images-storage.s3.ap-northeast-2.amazonaws.com/default_user_image.png"}
+                        // src={TinoIcon}
                          alt=""/>
                 </div>
                 <div className={"text-2xl font-bold"}>{log.userId}</div>
@@ -109,9 +109,7 @@ const LogComp = ({gameId, userId}) => {
         <>
             <div className={"mt-10 text-3xl mb-4"}>Top 10</div>
             <div className={"border-2 w-full h-80 rounded-3xl mb-4 flex"}>
-                <div className={"w-52 flex justify-center items-center"}>
-                    <OwnLogCardComp gameId={gameId} userId={userId}/>
-                </div>
+                <OwnLogCardComp gameId={gameId} userId={userId}/>
                 <LogCompListIntGameDetailPage gameId={gameId}/>
             </div>
         </>
--- src/hooks/useGetOneGame.js
+++ src/hooks/useGetOneGame.js
@@ -1,6 +1,9 @@
 import {GameController} from "../api/game.controller";
 import {useQuery} from "@tanstack/react-query";
 
+const MINUTE = 60 * 1000;
+const SECOND = 1000;
+
 export const useGetOneGame = ({gameId}) => {
 
     const findGameById = async () => {
@@ -11,6 +14,8 @@ export const useGetOneGame = ({gameId}) => {
     let {data: gameState, isFetching} = useQuery({
         queryKey: ["game", gameId],
         queryFn: findGameById,
+        staleTime: 5 * MINUTE,
+        gcTime: 5 * MINUTE,
     })
 
     return {gameState, isFetching};
