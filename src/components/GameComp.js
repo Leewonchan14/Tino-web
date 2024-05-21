8bafb0d70b98aab8fb49d17ef10b6a681c1c4d86
Won Chan Lee <twoone14@tukorea.ac.kr>
2024-05-21 14:12:57 +0900

    [refactor] GameComp refactoring

--- src/components/GameComp.js
+++ src/components/GameComp.js
@@ -2,37 +2,49 @@ import React, {useState} from 'react';
 import viewcount_icon from "../assets/viewcount_icon.png";
 import {useNavigate} from "react-router-dom";
 import SortMenuList from "./SortMenuList";
-import {useGetGames} from "../hooks/useGetGames";
-import {useInfiniteScroll} from "../hooks/useInfiniteScroll";
 import Spinner from "../assets/Spinner.gif";
 import {G_MARKET_FONT} from "../constant/FontFamily";
+import {GameController} from "../api/game.controller";
+
+const LoadingComp = ({loadingComp}) => {
+    return (
+        <div ref={loadingComp} className={"flex w-full justify-center"}>
+            <img src={Spinner} alt=""/>
+        </div>
+    )
+}
 
 export function GameCompList({...rest}) {
-    let [gameState, setGameState, findAllGame] = useGetGames();
     const [sortState, setSortState] = useState();
-    let [loadingComp, isLast, initScroll] = useInfiniteScroll(
-        gameState,
-        setGameState,
-        findAllGame,
-        {size: 9, sort: sortState},
-    )
+
+    const findAllGame = async ({page, size, sort}) => {
+        const response = await GameController.findAll({
+            page, size, sort
+        });
+        return response.data;
+    }
+
+
+    // let [loadingComp, isLast, initScroll] = useInfiniteScroll(
+    //     gameState,
+    //     setGameState,
+    //     findAllGame,
+    //     {size: 9, sort: sortState},
+    // )
     return (
         <>
-            <h1 className={"text-2xl font-bold"}>전체 게임
-                <span className={"text-blue-600"}> {gameState.length} 개</span>
+            <h1 className={"text-2xl font-bold mb-6"}>
+                전체 게임 {"gameState.length"} 개
             </h1>
-            <div className={"flex justify-end"}>
-                <SortMenuList setSortState={setSortState} initScroll={initScroll}/>
-            </div>
-            <div className={"grid grid-cols-3 gap-6 w-full"}>
-                {gameState.map((game, index) => {
-                    return <GameComp key={index} game={game}/>
-                })}
-            </div>
+            <SortMenuList setSortState={setSortState} className={""} initScroll={() => {
+            }}/>
+            <section className={"grid grid-cols-3 gap-6 w-full"}>
+                {/*{gameState.map((game, index) => {*/}
+                {/*    return <GameComp key={index} game={game}/>*/}
+                {/*})}*/}
+            </section>
 
-            {!isLast && <div ref={loadingComp} className={"flex w-full justify-center"}>
-                <img src={Spinner} alt=""/>
-            </div>}
+            <LoadingComp/>
 
         </>
     );
@@ -46,26 +58,26 @@ export function GameComp({game}) {
     }
 
     return (
-        <div onClick={onClick}
-             className={"border-2 border-gray-100 relative overflow-clip w-full rounded-2xl cursor-pointer bg-gray-50 flex-col"}>
-            <div className={"h-44 w-full"}>
+        <article onClick={onClick}
+                 className={"border-2 border-gray-100 relative overflow-clip w-full rounded-2xl cursor-pointer bg-gray-50 flex-col"}>
+            <picture className={"h-44 w-full"}>
                 <img className={"bg-gray-100 h-full w-full object-cover"}
                      src={game.gameImage} alt=""/>
-            </div>
-            <div className={"h-40 p-6 w-full flex-col flex"}>
+            </picture>
+            <section className={"h-40 p-6 w-full flex-col flex"}>
                 <h1 className={"text-xl"} style={{fontFamily: G_MARKET_FONT}}>
                     {game.gameName}
                 </h1>
-                <div className={"w-full mt-2 text-gray-600 flex-col flex"}>
-                    <div className={"line-clamp-2"}>{game.description}</div>
+                <div className={"w-full mt-2 text-gray-600 line-clamp-2"}>
+                    {game.description}
                 </div>
-                <div className={"mt-auto flex"}>
-                    <div className={"w-6 justify-center items-center flex"}>
+                <section className={"mt-auto flex"}>
+                    <picture className={"w-6 justify-center items-center flex"}>
                         <img src={viewcount_icon} alt=""/>
-                    </div>
+                    </picture>
                     <span className={"ml-4"}>{game.viewCount}</span>
-                </div>
-            </div>
-        </div>
+                </section>
+            </section>
+        </article>
     );
 }
--- src/hooks/useGetGames.js
+++ src/hooks/useGetGames.js
@@ -1,16 +1,8 @@
 import {useState} from "react";
-import {GameController} from "../api/game.controller";
 
 export const useGetGames = () => {
     const [gameState, setGameState] = useState([])
 
-    const findAllGame = async ({page, size, sort}) => {
-        const response = await GameController.findAll({
-            page, size, sort
-        });
-        setGameState([...gameState, ...response.data])
-        return response.data;
-    }
 
-    return [gameState, setGameState, findAllGame];
+    // return [gameState, setGameState, findAllGame];
 }
