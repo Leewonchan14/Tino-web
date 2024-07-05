a33121989329e758b231823a8182ccf03f2ef42a
Won Chan Lee <twoone14@tukorea.ac.kr>
2024-07-05 17:53:53 +0900

    [feat] 게임 sort 메뉴 크기 작게 조정

--- src/components/game/GameSortMenu.js
+++ src/components/game/GameSortMenu.js
@@ -1,30 +1,40 @@
 import React from "react";
-import {GAME_SORT_MENU} from "../../constants/Game";
+import { GAME_SORT_MENU } from "../../constants/Game";
 
-export const GameSortMenu = ({sortMenu, setSortMenu, className}) => {
-    const onMenuClick = (menu) => {
-        setSortMenu(menu);
-    }
+export const GameSortMenu = ({ sortMenu, setSortMenu, className }) => {
+  const onMenuClick = (menu) => {
+    setSortMenu(menu);
+  };
 
-    return (
-        <div className={"flex mb-10 justify-center gap-4"}>
-            {GAME_SORT_MENU.map((menu) => (
-                <MenuItem key={menu.value} menu={menu} sortMenu={sortMenu} onMenuClick={onMenuClick}/>
-            ))}
-        </div>
-    );
-}
+  return (
+    <div className={"flex mb-10 justify-center gap-3 mobile:gap-2"}>
+      {GAME_SORT_MENU.map((menu) => (
+        <MenuItem
+          key={menu.value}
+          menu={menu}
+          sortMenu={sortMenu}
+          onMenuClick={onMenuClick}
+        />
+      ))}
+    </div>
+  );
+};
 
-const MenuItem = ({menu,sortMenu, onMenuClick}) => {
-    return (
-        <button
-            onClick={() => onMenuClick(menu)}
-            className={"text-gray-600 w-32 px-4 py-3 mobile:py-2 text-center bg-gray-100 rounded-full hover:bg-primary-100 text-nowrap" +
-                " " +
-                (menu.value === sortMenu.value ? "bg-primary-600 text-white font-bold" : "")}>
-            {menu.name}
-        </button>
-    );
-}
+const MenuItem = ({ menu, sortMenu, onMenuClick }) => {
+  return (
+    <button
+      onClick={() => onMenuClick(menu)}
+      className={
+        "text-gray-600 w-24 mobile:w-20 px-2 mobile:px-2 py-3 mobile:py-2 text-center bg-gray-100 rounded-full hover:bg-primary-100 text-nowrap" +
+        " " +
+        (menu.value === sortMenu.value
+          ? "bg-primary-600 text-white font-bold"
+          : "")
+      }
+    >
+      {menu.name}
+    </button>
+  );
+};
 
 export default GameSortMenu;
