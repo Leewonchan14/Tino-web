95e60113f8ab3ea102302bfe2fa29e850e583394
이원찬 <twoone14@tukorea.ac.kr>
2024-07-15 14:47:09 +0900

    [chore] tailwind.config 마이페이지 설정

--- src/index.js
+++ src/index.js
@@ -1,20 +1,23 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import reportWebVitals from "./reportWebVitals";
 import App from "./App";
-import {QueryClient, QueryClientProvider} from '@tanstack/react-query'
-import {ReactQueryDevtools} from "@tanstack/react-query-devtools";
+import {
+  QueryClient,
+  QueryClientProvider,
+} from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 
 const queryClient = new QueryClient();
 
 root.render(
-    <QueryClientProvider client={queryClient}>
-        <ReactQueryDevtools /> {/* 선택 사항 */}
-        <App/>
-    </QueryClientProvider>
+  <QueryClientProvider client={queryClient}>
+    <ReactQueryDevtools /> {/* 선택 사항 */}
+    <App />
+  </QueryClientProvider>
 );
 
 // If you want to start measuring performance in your app, pass a function
--- tailwind.config.js
+++ tailwind.config.js
@@ -10,9 +10,14 @@ module.exports = {
       },
       screens: {
         mobile: { min: "240px", max: "640px" },
+        pc: { min: "1280px" },
         sm: { min: "641px", max: "819px" },
         md: { min: "820px", max: "1023px" },
         lg: { min: "1080px" },
+      },
+      spacing: {
+        "my-page-max-width": "764px",
+        "home-max-width": "1080px",
       },
       fontFamily: {
         G_MARKET: "GmarketSans",
